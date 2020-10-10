4fc080f0d192ed33b6e17070da51abb690274cb1
Everton Braga <evertonr.braga@gmail.com>
2020-10-10 12:26:20 -0300

    Regex

--- index.js
+++ index.js
@@ -25,3 +25,20 @@ const content00 = 'Repeat, Repeat, Repeat';
 //select both of them using 'i' and 'g' (insensitive / global)
 const regex00 = /twinkle/gi;
 const content00 = 'Twinkle, twinkle, little star';
+
+//select both of the words initializing with 'hu';
+const regex00 = /hu./g;
+const content00 = "I'll hum a song";
+const content00 = 'Bear hug';
+
+//select both of them using 'i' and 'g' (insensitive / global)
+const regex00 = /.un/;
+const content00 = "Let's have fun with regular expressions!";
+
+//select both of them using 'i' and 'g' (insensitive / global)
+const regex00 = /.un/;
+const content00 = "Let's have fun with regular expressions!";
+
+//select words containing any char between the brackets
+const regex00 = /b[aou]g/;
+const content00 = 'bug, bag';
